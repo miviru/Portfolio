c724012c4000151e7e9f7429919820872fdf91d3
miviru <miviru98@hotmail.com>
2025-01-14 17:54:51 +0100

    Initial commit

---
+++ wwwroot/js/particles-config.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+    particlesJS("particles-js", {
+        particles: {
+            number: {
+                value: 100,
+                density: {
+                    enable: true,
+                    value_area: 800,
+                },
+            },
+            color: {
+                value: "#ffffff", // Color de las partículas
+            },
+            shape: {
+                type: "circle",
+            },
+            opacity: {
+                value: 0.5,
+            },
+            size: {
+                value: 3,
+                random: true,
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1,
+            },
+            move: {
+                enable: true,
+                speed: 2,
+                direction: "none",
+                out_mode: "out",
+            },
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "repulse",
+                },
+                onclick: {
+                    enable: true,
+                    mode: "push",
+                },
+            },
+            modes: {
+                repulse: {
+                    distance: 100,
+                    duration: 0.4,
+                },
+                push: {
+                    particles_nb: 4,
+                },
+            },
+        },
+        retina_detect: true,
+    });
+});
