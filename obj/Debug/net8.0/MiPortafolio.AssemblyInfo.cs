d2beb0f2f4da638233c5e1dada585089b83c2612
miviru <miviru98@hotmail.com>
2025-01-14 18:12:05 +0100

    dockerfile

--- obj/Debug/net8.0/MiPortafolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiPortafolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiPortafolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c724012c4000151e7e9f7429919820872fdf91d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiPortafolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiPortafolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
