a0619a918edfeab9c75b3bf3988ff99e83f676a8
miviru <miviru98@hotmail.com>
2025-01-16 16:29:07 +0100

    cv ajustado

--- obj/Debug/net8.0/MiPortafolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiPortafolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiPortafolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7b1ac1cd2690e030ac99f999d7912e8f20d1a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2a5aa6f15b0d28156df607c5a2194c4299660e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiPortafolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiPortafolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
